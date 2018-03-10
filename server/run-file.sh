803685675b68f4ecafc3785c93bac453e7415ec2
Adam Pridgen <adam.pridgen@thecoverofnight.com>
2018-03-10 14:12:14 -0600

    working on trying to get elasticsearch running

--- server/run-file.sh
+++ server/run-file.sh
@@ -4,4 +4,7 @@ DOCKER_NAME=fluent-capture
 docker kill $DOCKER_NAME
 docker rm $DOCKER_NAME
 docker build -t $DOCKER_TAG .
-docker run -d -p 9880:9880 -p 20001:20001 -v /home/dso/fluentd:/fluentd/etc -e FLUENTD_CONF=fluent.conf --name $DOCKER_NAME $DOCKER_TAG
+#docker run -d -p 9880:9880 -p 20001:20001 -p 42100:42100/udp \
+#   -v /home/dso/fluentd:/fluentd/etc -e FLUENTD_CONF=fluent.conf --name $DOCKER_NAME $DOCKER_TAG
+docker run -d -p 9880:9880 -p 20001:20001 -p 42100:42100/udp \
+    -e FLUENTD_CONF=fluent.conf --name $DOCKER_NAME $DOCKER_TAG
