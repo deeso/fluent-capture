8aed1f4b86dcbc92820204d95539d123f5a9e9e0
Adam Pridgen <adam.pridgen@thecoverofnight.com>
2018-03-15 05:39:55 -0500

    saving the current fluent configuration

--- capture/run-file.sh
+++ capture/run-file.sh
@@ -7,4 +7,5 @@ docker build -t $DOCKER_TAG .
 #docker run -d -p 9880:9880 -p 20001:20001 -p 42100:42100/udp \
 #   -v /home/dso/fluentd:/fluentd/etc -e FLUENTD_CONF=fluent.conf --name $DOCKER_NAME $DOCKER_TAG
 docker run -d -p 9880:9880 -p 20001:20001 -p 42100:42100/udp \
+              -p 42101:42101/udp \
     -e FLUENTD_CONF=fluent.conf --name $DOCKER_NAME $DOCKER_TAG
